c9deac9f54a8e84f0b1d14b4bbd3ba775aeae52e
Willem van Ketwich <willvk@gmail.com>
2018-10-20 15:01:57 +1100

    initial framework for testing

---
+++ README.md
@@ -0,0 +1,9 @@
+# python library for interacting with gdbserver
+
+# paths:
+
+`bin` - host run scripts
+`docker` - docker stuff
+`scripts` - scripts run from inside a container
+`tests` - test suite
+
---
+++ tests/test_learn_gdbs.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python
+
+from nose import with_setup
+
+class TestGdbServerProtocol:
+
+    @classmethod
+    def setup_class(self):
+        pass
+
+    def test_can_get_response(self):
+        return False
